40a0ae9dd5b7cf7b580705daf71f00479e407499
micky225 <mickytod2255@gmail.com>
2024-02-27 12:06:55 +0000

    updated

--- User_Password_Reset/settings.py
+++ User_Password_Reset/settings.py
@@ -151,7 +151,7 @@ EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
 EMAIL_HOST_USER= 'mickytod03@gmail.com'
-EMAIL_HOST_PASSWORD= KEY
+EMAIL_HOST_PASSWORD= "eumb huxh utax envr"
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
